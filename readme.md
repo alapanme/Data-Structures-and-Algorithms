3256bace13ca6264cce657db39d8f2fe8f0e8869
alapanme <25705483+alapanme@users.noreply.github.com>
2022-03-23 20:45:34 +0200

    kadanes algo on(n) solution

--- readme.md
+++ readme.md
@@ -10,6 +10,7 @@ In this project I will write java programmes for various Data Structures and Alg
 - [Two Sum](https://github.com/alapanme/Data-Structures-and-Algorithms/blob/master/src/com/alapan/dsalgo/leetcode/twoSumSolution.java)
 - [Reverse Integer](https://github.com/alapanme/Data-Structures-and-Algorithms/blob/master/src/com/alapan/dsalgo/leetcode/reverseIntegerSolution.java)
 - [Palindrome Number](https://github.com/alapanme/Data-Structures-and-Algorithms/blob/master/src/com/alapan/dsalgo/leetcode/palindromeSolution.java)
+- [Maximum SubArray(Kadane's Algorithm)](https://github.com/alapanme/Data-Structures-and-Algorithms/blob/master/src/com/alapan/dsalgo/leetcode/maximumSubArrayKadanesAlgo.java)
 
 # Practice Solutions
 - [Reverse String](https://github.com/alapanme/Data-Structures-and-Algorithms/blob/master/src/com/alapan/dsalgo/practice/reverseString.java)
--- src/com/alapan/dsalgo/leetcode/maximumSubArrayKadanesAlgo.java
+++ src/com/alapan/dsalgo/leetcode/maximumSubArrayKadanesAlgo.java
@@ -4,11 +4,11 @@ public class maximumSubArrayKadanesAlgo {
 
     public int bruteForceSolution(int[] a) { //Time Complexity is O(n^2)
         int max = Integer.MIN_VALUE;
-        for (int i=0; i<a.length; i++){
+        for (int i = 0; i < a.length; i++) {
             int sum = 0;
-            for (int j=i; j<a.length; j++){
-                sum = sum+a[j];
-                if (sum > max){
+            for (int j = i; j < a.length; j++) {
+                sum = sum + a[j];
+                if (sum > max) {
                     max = sum;
                 }
             }
@@ -16,8 +16,23 @@ public class maximumSubArrayKadanesAlgo {
         return max;
     }
 
+    public int optimizedSolution(int[] a) { //Time Complexity is O(n)
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+        for (int i = 0; i < a.length; i++) {
+            if (sum < 0) {
+                sum = a[i];
+            } else {
+                sum = sum + a[i];
+                max = Math.max(sum, max);
+            }
+        }
+        return max;
+    }
+
     public static void main(String[] args) {
         maximumSubArrayKadanesAlgo ka = new maximumSubArrayKadanesAlgo();
-        System.out.println(ka.bruteForceSolution(new int[]{-2,1,-3,4,-1,2,1,-5,4}));
+        System.out.println(ka.bruteForceSolution(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
+        System.out.println(ka.optimizedSolution(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
     }
 }
