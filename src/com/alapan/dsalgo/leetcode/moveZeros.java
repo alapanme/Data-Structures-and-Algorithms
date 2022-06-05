e4b90bbb0b40892c3d51093dd74866e8ccfd9f7d
alapanme <25705483+alapanme@users.noreply.github.com>
2022-06-05 08:18:05 +0300

    Move Zeros

--- src/com/alapan/dsalgo/leetcode/moveZeros.java
+++ src/com/alapan/dsalgo/leetcode/moveZeros.java
@@ -1,3 +1,17 @@
+//https://leetcode.com/problems/move-zeroes/
+/*
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+Note that you must do this in-place without making a copy of the array.
+
+Example 1:
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+
+Example 2:
+Input: nums = [0]
+Output: [0]
+ */
+
 package com.alapan.dsalgo.leetcode;
 
 import java.util.ArrayList;
@@ -28,12 +42,12 @@ public class moveZeros {
 
         int index = 0;
 
-        for(int i=0; i<nums.length; i++){
-            if(nums[i] != 0){
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
                 nums[index++] = nums[i];
             }
         }
-        for(int j=index; j<nums.length; j++){
+        for (int j = index; j < nums.length; j++) {
             nums[j] = 0;
         }
         System.out.println(Arrays.toString(nums));
